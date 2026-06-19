ff8f0d864beb16e8dd71b16149c307d2f3fd91f5
sofiaversoza <dev.sofverz@gmail.com>
2026-06-19 15:52:32 -0700

    Redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,49 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/index.html",
-        destination: "/",
-        permanent: true,
-      },
-    ]
-  },
+	async redirects() {
+		return [
+			{
+				source: "/index.html",
+				destination: "/",
+				permanent: true,
+			},
+			{
+				source: "/vascular-physicians.html",
+				destination: "/physicians",
+				permanent: true,
+			},
+			{
+				source: "/contact.html",
+				destination: "/contact",
+				permanent: true,
+			},
+			{
+				source: "/jeromy-brink.html",
+				destination: "/physicians/jeromy-brink",
+				permanent: true,
+			},
+			{
+				source: "/hospitals-facilities.html",
+				destination: "/resources/hospital-members",
+				permanent: true,
+			},
+			{
+				source: "/vein-center.html",
+				destination: "/about/vein-center",
+				permanent: true,
+			},
+			{
+				source: "/vascular-faq.html",
+				destination: "/resources/faq",
+				permanent: true,
+			},
+			{
+				source: "/new-patient.html",
+				destination: "/resources",
+				permanent: true,
+			},
+		]
+	},
 }
 
 export default nextConfig
